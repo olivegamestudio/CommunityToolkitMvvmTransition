0a5f3668e1d3c7df4dd78db37481c18f36c1d967
Simon Jefferies <simon.jefferies@inseinc.com>
2025-09-25 10:50:05 +0100

    Add two projects to demo traditional WPF command pattern and the CommunityToolkit way.

---
+++ CommunityToolkitMvvmCommands/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace CommunityToolkitMvvmCommands;
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+	public MainWindow()
+	{
+		InitializeComponent();
+		DataContext = new MenuViewModel();
+	}
+
+	public partial class MenuViewModel : ObservableObject
+	{
+		[ObservableProperty]
+		int _pressCount;
+
+		[RelayCommand]
+		public void PressMe()
+		{
+			PressCount++;
+			MessageBox.Show("Pressed");
+		}
+	}
+}
---
+++ TraditionalCommands/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Input;
+
+namespace TraditionalCommands;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+	public MainWindow()
+	{
+		InitializeComponent();
+		DataContext = new MenuViewModel();
+	}
+}
+
+public class PressMeCommand : ICommand
+{
+	readonly MenuViewModel _viewModel;
+
+	public PressMeCommand(MenuViewModel viewModel)
+	{
+		_viewModel = viewModel;
+	}
+
+	public bool CanExecute(object? parameter)
+	{
+		return true;
+	}
+
+	public void Execute(object? parameter)
+	{
+		_viewModel.PressCount++;
+		MessageBox.Show("Pressed");
+	}
+
+	public event EventHandler? CanExecuteChanged;
+}
+
+public class MenuViewModel : INotifyPropertyChanged
+{
+	public ICommand PressMe { get; set; }
+
+	int _pressCount;
+
+	public int PressCount
+	{
+		get => _pressCount;
+		set
+		{
+			_pressCount = value;
+			OnPropertyChanged(nameof(PressCount));
+		}
+	}
+
+	public MenuViewModel()
+	{
+		PressMe = new PressMeCommand(this);
+	}
+
+	public event PropertyChangedEventHandler? PropertyChanged;
+
+	protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+	{
+		PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+	}
+}
